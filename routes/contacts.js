c7e1ae74d1417a602ef31612148f3ff8039c2653
nikulasoskarsson <nikulasoskarsson@gmail.com>
2019-12-15 15:54:02 +0000

    Final product ready for production build

--- routes/contacts.js
+++ routes/contacts.js
@@ -98,26 +98,22 @@ router.put('/:id', auth, async (req, res) => {
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.put('/:id', auth, async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   try {
     let contact = await Contact.findById(req.params.id);
 
     if (!contact) return res.status(404).json({ msg: 'Contact not found' });
-    // Make sure users owns contact
+
+    // Make sure user owns contact
     if (contact.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'Not authorized' });
     }
 
-    contact = await Contact.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: contactFields
-      },
-      { new: true }
-    );
-    res.json(contact);
-  } catch (error) {
-    console.error(error.message);
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Contact removed' });
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
